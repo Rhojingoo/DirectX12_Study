dfb6a06669657333caf3a1facbd5b65e50538b18
rhojingoo <rhojingoo@naver.com>
2024-05-07 16:52:48 +0900

    업데이트

--- unzip_fbx_Debug.bat
+++ unzip_fbx_Debug.bat
@@ -1,8 +1,10 @@
 @echo off
 
 REM
-SET "ARCHIVE_PATH=D:\DirectX12_Study\Library\Lib\debug.zip"
-SET "DEST_PATH=D:\DirectX12_Study\Library\Lib\FBX\debug"
+SET "BASE_PATH=%~dp0"
+SET "ARCHIVE_PATH=%BASE_PATH%Library\Lib\debug.zip"
+SET "DEST_PATH=%BASE_PATH%Library\Lib\FBX\debug"
+
 
 REM
 powershell Expand-Archive -Path "%ARCHIVE_PATH%" -DestinationPath "%DEST_PATH%" -Force
--- unzip_fbx_Release.bat
+++ unzip_fbx_Release.bat
@@ -1,8 +1,9 @@
 @echo off
 
 REM
-SET "ARCHIVE_PATH=D:\DirectX12_Study\Library\Lib\release.zip"
-SET "DEST_PATH=D:\DirectX12_Study\Library\Lib\FBX\release"
+SET "BASE_PATH=%~dp0"
+SET "ARCHIVE_PATH=%BASE_PATH%Library\Lib\release.zip"
+SET "DEST_PATH=%BASE_PATH%Library\Lib\FBX\release"
 
 REM
 powershell Expand-Archive -Path "%ARCHIVE_PATH%" -DestinationPath "%DEST_PATH%" -Force
